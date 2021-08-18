400bd9890e5f80fa64bc39ba374a261f1ec52e46
windro-ser <88357206+windro-ser@users.noreply.github.com>
2021-08-18 14:44:57 +0530

    Update Owner git.js

--- plugins/Owner git.js
+++ plugins/Owner git.js
@@ -1,29 +1,56 @@
+/* Copyright (C) 2021 Ameer Suhail
+CODDED Ameer Suhail
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Pikachu
+*/
+
 const Asena = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-const axios = require('axios');
+const {MessageType} = require('@adiwajshing/baileys');
+const {spawnSync} = require('child_process');
+const Config = require('../config');
+const chalk = require('chalk');
 
 const Language = require('../language');
-const Lang = Language.getString('wallpaper');
-
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
-
-    var r_text = new Array ();
+const Lang = Language.getString('system_stats');
 
 
+if (Config.WORKTYPE == 'private') {
 
-  r_text[0] = "https://raw.githubusercontent.com/windro-ser/Jarvis-V2/main/bot.png";
+    Asena.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
+        if (message.jid === '15369524516-1612300121@g.us') {
 
-    var i = Math.floor(1*Math.random())
+            return;
+        }
 
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
-
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater WindrO*
+        if (Config.PLK == 'default') {
+            await message.client.sendMessage(message.jid,'*Creater WindrO*
 *Bgm updation💘*
 *Owner number wa.me/918606413490*
 *githublink       https://github.com/windro-ser/Jarvis-V2*
 *audio commads    https://github.com/windro-ser/Jarvis-V2/tree/main/uploads*
-*sticker commads  https://github.com/windro-ser/Jarvis-V2/tree/main/stickers*
-`})
+*sticker commads  https://github.com/windro-ser/Jarvis-V2/tree/main/stickers*' , MessageType.text);
+        }
+    }));
+}
 
-}));
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+
+        if (message.jid === '54218542512-1612300121@g.us') {
+
+            return;
+        }
+
+        if (Config.PLK == 'default') {
+            await message.client.sendMessage(message.jid,'*Creater WindrO*
+*Bgm updation💘*
+*Owner number wa.me/918606413490*
+*githublink       https://github.com/windro-ser/Jarvis-V2*
+*audio commads    https://github.com/windro-ser/Jarvis-V2/tree/main/uploads*
+*sticker commads  https://github.com/windro-ser/Jarvis-V2/tree/main/stickers*' , MessageType.text);
+        }
+    }));
+}
